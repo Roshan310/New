4c44b8e738cd8b3c731f9af58eec2c90c8d1579b
Roshan Aryal <64967304+Roshan310@users.noreply.github.com>
2023-05-20 18:55:16 +0545

    api_key hidden

--- store_address.py
+++ store_address.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 PATH = 'https://api.polygonscan.com/api'
 ADDRESS = '0xf57C5d032a0Eb0b9e9a2081F4b7992bed90336D3'
 CONTRACT = '0xAe07B360cF41C8971F6c544620A6ed428Ff3a661'
-API_KEY = 'VQJKJPPTQDCM8H3YWQ7XNJSI59BHWGGCYQ'
+API_KEY = 'dummy'
 
 # response = requests.get(f'{PATH}?module=account&action=tokentx&contractaddress={CONTRACT}&address={ADDRESS}&startblock=0&endblock=99999999&page=10&offset=5&sort=asc&apikey={API_KEY}')
 
